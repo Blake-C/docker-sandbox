114e645b87cd25ec772ecc2b2e3ef2c42593e14e
Blake Cerecero <Blake@DigitalBlake.com>
2017-01-10 01:11:14 -0600

    Initial Commit

---
+++ public/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Hello</title>
+
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.0/css/foundation.min.css">
+</head>
+<body>
+	<div class="row">
+		<div class="medium-12 columns">
+			<h1>Hello World</h1>
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt debitis, nihil quae labore nisi molestiae perspiciatis quam commodi voluptas perferendis nam veniam saepe ipsa doloribus tempore, mollitia suscipit ipsam! Ipsa!</p>
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt debitis, nihil quae labore nisi molestiae perspiciatis quam commodi voluptas perferendis nam veniam saepe ipsa doloribus tempore, mollitia suscipit ipsam! Ipsa!</p>
+
+			<hr>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="medium-6 columns">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt debitis, nihil quae labore nisi molestiae perspiciatis quam commodi voluptas perferendis nam veniam saepe ipsa doloribus tempore, mollitia suscipit ipsam! Ipsa!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt debitis, nihil quae labore nisi molestiae perspiciatis quam commodi voluptas perferendis nam veniam saepe ipsa doloribus tempore, mollitia suscipit ipsam! Ipsa!</p>
+		</div>
+		<div class="medium-6 columns">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt debitis, nihil quae labore nisi molestiae perspiciatis quam commodi voluptas perferendis nam veniam saepe ipsa doloribus tempore, mollitia suscipit ipsam! Ipsa!</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt debitis, nihil quae labore nisi molestiae perspiciatis quam commodi voluptas perferendis nam veniam saepe ipsa doloribus tempore, mollitia suscipit ipsam! Ipsa!</p>
+		</div>
+	</div>
+
+	<div class="row columns">
+		<?php
+			phpinfo();
+		?>
+	</div>
+</body>
+</html>
