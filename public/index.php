7cbddff59e21986f3590b26cdaa68c384ecd3d51
Blake Cerecero <Blake@DigitalBlake.com>
2017-01-12 02:00:31 -0600

    [htaccess, php, css] Added additional assets to serve
    Signed-off-by: Blake Cerecero <Blake@DigitalBlake.com>

--- public/index.php
+++ public/index.php
@@ -5,15 +5,17 @@
 	<title>Hello</title>
 
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.0/css/foundation.min.css">
+	<link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
 	<div class="row">
 		<div class="medium-12 columns">
 			<h1>Todo List</h1>
 			<ul>
-				<li>Export sql file on container stop</li>
+				<li><del>Export sql file on container stop</del></li>
 				<li><del>Dynamic ports when provided one is in use</del></li>
 				<li>Create custom image with tools needed for WordPress development</li>
+				<li>Serve static assets</li>
 			</ul>
 		</div>
 	</div>
